020f38168ec3a5bcaff079e126aa369f7b27a160
vyneer <vyneer@protonmail.com>
2021-05-28 21:51:24 +0300

    * idk im giving up

--- src/main.rs
+++ src/main.rs
@@ -61,85 +61,88 @@ async fn main() {
 
     let regex = Regex::new(r"(^|\s)((#twitch|#twitch-vod|#twitch-clip|#youtube|#youtube-live)/(?:[A-z0-9_\-]{3,64}))\b").unwrap();
 
-    let ws = connect_async(Url::parse("wss://chat.destiny.gg/ws").unwrap());
+    loop {
+        let ws = connect_async(Url::parse("wss://chat.destiny.gg/ws").unwrap());
 
-    let (socket, response) = match timeout(Duration::from_secs(10), ws).await {
-        Ok(ws) => {
-            let (socket, response) = match ws {
-                Ok((socket, response)) => {
-                    if response.status() != 101 {
-                        panic!("Response isn't 101, can't continue.")
-                    }
-                    (socket, response)
-                },
-                Err(e) => {
-                    panic!("Unexpected error: {}", e)
-                }
-            };
-            (socket, response)
-        },
-        Err(_) => panic!("Connection timed out, panicking.")
-    };
-    
-    info!("Connected to the server");
-    debug!("Response HTTP code: {}", response.status());
-
-    let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
-
-    let (write, read) = socket.split();
-
-    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
-    let ws_to_stdout  = {
-        read.for_each(|msg| async {
-            let msg_og = match msg {
-                Ok(msg_og) => msg_og,
-                Err(tokio_tungstenite::tungstenite::Error::Io(e)) => {
-                    panic!("Tungstenite IO error, panicking: {}", e);
-                },
-                Err(e) => {
-                    panic!("Some kind of other error occured, panicking: {}", e);
-                }
-            };
-            if msg_og.is_text() {
-                let (msg_type, msg_data) = split_once(msg_og.to_text().unwrap());
-                match msg_type {
-                    "MSG" => {
-                        let msg_des: Message = serde_json::from_str(&msg_data).unwrap();
-                        let capt = regex.captures_iter(msg_des.data.as_str());
-                        let mut capt_vector = Vec::new();
-                        for result in capt {
-                            capt_vector.push(result[2].to_string());
+        let (socket, response) = match timeout(Duration::from_secs(10), ws).await {
+            Ok(ws) => {
+                let (socket, response) = match ws {
+                    Ok((socket, response)) => {
+                        if response.status() != 101 {
+                            panic!("Response isn't 101, can't continue.")
                         }
-                        if capt_vector.len() != 0 {
-                            capt_vector.dedup();
-                            for mut result in capt_vector {
-                                if result.contains("#twitch/") {
-                                    result = result.to_lowercase();
-                                }
-                                conn.execute("INSERT INTO embeds (timest, link) VALUES (?1, ?2)", params![msg_des.timestamp/1000, result]).unwrap();
-                                debug!("Added embed to db: {}", result);
-                            }
-                        }
+                        (socket, response)
                     },
-                    _ => (),
+                    Err(e) => {
+                        panic!("Unexpected error: {}", e)
+                    }
+                };
+                (socket, response)
+            },
+            Err(_) => panic!("Connection timed out, panicking.")
+        };
+        
+        info!("Connected to the server");
+        debug!("Response HTTP code: {}", response.status());
+    
+        let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
+    
+        let (write, read) = socket.split();
+    
+        let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+        let ws_to_stdout  = {
+            let reader = read.for_each(|msg| async {
+                let msg_og = match msg {
+                    Ok(msg_og) => msg_og,
+                    Err(tokio_tungstenite::tungstenite::Error::Io(e)) => {
+                        panic!("Tungstenite IO error, panicking: {}", e);
+                    },
+                    Err(e) => {
+                        panic!("Some kind of other error occured, panicking: {}", e);
+                    }
+                };
+                if msg_og.is_text() {
+                    let (msg_type, msg_data) = split_once(msg_og.to_text().unwrap());
+                    match msg_type {
+                        "MSG" => {
+                            let msg_des: Message = serde_json::from_str(&msg_data).unwrap();
+                            let capt = regex.captures_iter(msg_des.data.as_str());
+                            let mut capt_vector = Vec::new();
+                            for result in capt {
+                                capt_vector.push(result[2].to_string());
+                            }
+                            if capt_vector.len() != 0 {
+                                capt_vector.dedup();
+                                for mut result in capt_vector {
+                                    if result.contains("#twitch/") {
+                                        result = result.to_lowercase();
+                                    }
+                                    conn.execute("INSERT INTO embeds (timest, link) VALUES (?1, ?2)", params![msg_des.timestamp/1000, result]).unwrap();
+                                    debug!("Added embed to db: {}", result);
+                                }
+                            }
+                        },
+                        _ => (),
+                    }
                 }
-            }
-            if msg_og.is_ping() {
-                debug!("{:?}", Pong(msg_og.clone().into_data()));
-                stdin_tx.unbounded_send(Pong(msg_og.clone().into_data())).unwrap();
-            }
-            if msg_og.is_close() {
-                panic!("Server closed the connection, panicking.")
-            }
-        })
-    };
-
-    /*thread::spawn(move || {
-        stdin_tx.unbounded_send(Ping("ping".as_bytes().to_vec())).unwrap();
-
-        thread::sleep(Duration::from_secs(5));
-    }); */
-
-    pin_mut!(stdin_to_ws, ws_to_stdout);
-    future::select(stdin_to_ws, ws_to_stdout).await;
+                if msg_og.is_ping() {
+                    debug!("{:?}", Pong(msg_og.clone().into_data()));
+                    stdin_tx.unbounded_send(Pong(msg_og.clone().into_data())).unwrap();
+                }
+                if msg_og.is_close() {
+                    panic!("Server closed the connection, panicking.")
+                }
+            });
+            timeout(Duration::from_secs(60*2), reader)
+        };
+    
+        /*thread::spawn(move || {
+            stdin_tx.unbounded_send(Ping("ping".as_bytes().to_vec())).unwrap();
+    
+            thread::sleep(Duration::from_secs(5));
+        }); */
+    
+        pin_mut!(stdin_to_ws, ws_to_stdout);
+        future::select(stdin_to_ws, ws_to_stdout).await;
+    }
 }
